2249e8b4024b81db4f9e8a84e0786a8a80b77917
frsiam <siam.frs@gmail.com>
2022-03-01 12:59:48 +0600

    add the card into the html file

--- js/app.js
+++ js/app.js
@@ -12,15 +12,27 @@ const foundMessage = document.getElementById('not-found')
 // load data which brand name you search 
 
 const searchPhone = () => {
-    const inputText = document.getElementById('search-phone').value
+    const inputText = (document.getElementById('search-phone').value).toLowerCase()
     if(inputText == ''){
         showError('Please input brand name')
     }
-    else{
-        const url = `https://openapi.programming-hero.com/api/phones?search=${inputText.toLowerCase()}`
+    else if(inputText == 'iphone' || inputText == 'samsung' || inputText == 'oppo' || inputText == 'huawei'){
+        foundMessage.textContent = ''
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
 
         fetch(url)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => showData(data))
     }
+    else{
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <h2 class="text-danger border border-2 border-warning fw-bolder px-5 py-3">No phone found, which you searching</h2>
+        `
+        foundMessage.appendChild(div)
+    }
+}
+
+const showData = data => {
+    console.log(data.data)
 }
