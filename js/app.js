541c87d9ae7f577af998a1c77794d1cd0423ea9e
frsiam <siam.frs@gmail.com>
2022-03-01 16:01:46 +0600

    show the model name and brand name

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ function showError(errorMessage) {
 
 // not found text id 
 const foundMessage = document.getElementById('not-found')
+// show the phone into the card div 
+const phoneCard = document.getElementById('phone-card')
+// more button div 
+const moreBtn = document.getElementById('more-phone')
 
 // load data which brand name you search 
 
@@ -32,29 +36,34 @@ const searchPhone = () => {
         foundMessage.appendChild(div)
     }
 }
-const phoneCard = document.getElementById('phone-card')
+
 const showData = data => {
+
     phoneCard.textContent = ''
+    moreBtn.textContent = ''
+
     console.log(data)
+    // get 20 data 
     const twentyData = data.slice(0,20)
+    // remaining data 
     const remainingData =data.length - twentyData.length
-    console.log(remainingData)
+
     if(data.length <= 20) {
         console.log(twentyData.length)
         twentyData.forEach(element => {
+            console.log(element.phone_name)
             const cardDiv = document.createElement('div')
             cardDiv.className = 'col'
             cardDiv.innerHTML = `
             <div class="card h-100">
                 <img src="${element.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a short card.</p>
+                <h5 class="card-title"><span class="fw-bold fs-5 text-primary">Model Name :</span> ${element.phone_name}</h5>
+                <p class="card-text fw-bold"><span class="fw-bold fs-5 text-primary">Brand :</span> ${element.brand}</p>
                 </div>
             </div>
             `
             phoneCard.appendChild(cardDiv)
-            console.log(phoneCard)
         });
     }
     else{
@@ -64,20 +73,32 @@ const showData = data => {
             cardDiv.className = 'col'
             cardDiv.innerHTML = `
             <div class="card h-100">
-                <img src="${element.image}" class="card-img-top" alt="...">
+                <img src="${element.image}" class="card-img-top w-75" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a short card.</p>
+                <h5 class="card-title"><span class="fw-bold fs-5 text-primary">Model Name :</span> ${element.phone_name}</h5>
+                <p class="card-text fw-bold"><span class="fw-bold fs-5 text-primary">Brand :</span> ${element.brand}</p>
                 </div>
             </div>
             `
             phoneCard.appendChild(cardDiv)
-            console.log(phoneCard)
         });
-        const moreBtn = document.getElementById('more-phone')
+        /* const moreDataShow = () => {
+            remainingData.forEach(element =>{
+                cardDiv.innerHTML = `
+                <div class="card h-100">
+                    <img src="${element.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a short card.</p>
+                    </div>
+                </div>
+                `
+                phoneCard.appendChild(cardDiv)
+            })
+        } */ 
         const moreDiv = document.createElement('div')
         moreDiv.innerHTML = `
-        <button class="btn btn-outline-dark">More Phone</button>
+        <button onclick='moreDataShow()' class="btn btn-outline-dark">More Phone</button>
         `
         moreBtn.appendChild(moreDiv)
     } 
