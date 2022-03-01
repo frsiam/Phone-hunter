861fe45b72469c7ea29d08981fba2798a2411ae5
frsiam <siam.frs@gmail.com>
2022-03-01 12:12:27 +0600

    input field value catch and if empty then show an alert, then by valid text we make a url and then show the in the consol

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
-function check() {
+function showError(errorMessage) {
     Swal.fire({
         icon:'error',
-        title: 'Error',
-        text: 'errorMessage'
+        title: 'Warning',
+        text: errorMessage
     })
+}
+
+// not found text id 
+const foundMessage = document.getElementById('not-found')
+
+// load data which brand name you search 
+
+const searchPhone = () => {
+    const inputText = document.getElementById('search-phone').value
+    if(inputText == ''){
+        showError('Please input brand name')
+    }
+    else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputText.toLowerCase()}`
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
 }
